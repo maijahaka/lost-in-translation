242a4130291cbefd3462204226e7c8416c102f0f
jessemaa <mewwbae@gmail.com>
2021-03-03 17:15:08 +0200

    created skeleton for profile-view and added seperation to translations/cards

--- src/components/containers/Profile.js
+++ src/components/containers/Profile.js
@@ -1,8 +1,18 @@
-const Profile = () => {
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+import Translations from '../translations/Translations'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function Profile() {
   return (
-    <div>
-      <p>Profile page</p>
-    </div>
+    <Container>
+      <Jumbotron>
+        <h1>Hello, {"yourName"}</h1>
+        <p>Here are your ten most recent translations listed below.</p>
+      </Jumbotron>
+      <Translations />
+    </Container>
   )
 }
 
---
+++ src/components/translations/Translations.js
@@ -0,0 +1,11 @@
+import TranslateCard from './TranslateCard'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function Translations() {
+  return (
+      <TranslateCard />
+  )
+}
+
+export default Translations
