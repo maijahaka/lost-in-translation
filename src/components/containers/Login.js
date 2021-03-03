bae5c39c46b716b823998eaafb3de48866701e03
Maija Haka <maija.haka@live.fi>
2021-03-03 19:07:21 +0200

    feat: implement login functionality

--- src/components/containers/Login.js
+++ src/components/containers/Login.js
@@ -2,16 +2,24 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
 import LoginForm from '../forms/LoginForm'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { useHistory } from 'react-router-dom'
 
 
 function Login() {
+  let history = useHistory()
+
+  const handleLogin = (user) => {
+    window.localStorage.setItem('user', user)
+    history.push('/translate')
+  }
+
   return (
     <Container>
         <Jumbotron>
             <h1>Lost in Translation!</h1>
             <h2>Get started</h2>
             <br />
-            <LoginForm />
+            <LoginForm login={handleLogin} />
         </Jumbotron>
     </Container>
   )
