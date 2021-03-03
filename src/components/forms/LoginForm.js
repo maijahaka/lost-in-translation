023b031a53be32b7a583c51f1fbb3100d253d4d0
jessemaa <mewwbae@gmail.com>
2021-03-03 16:11:24 +0200

    seperated login form and login-view

---
+++ src/components/forms/LoginForm.js
@@ -0,0 +1,16 @@
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function Login() {
+  return (
+      <div>
+        <Form.Control size="lg" type="text" placeholder="What's your name?" />
+        <br />
+        <Button variant="success">Start</Button>
+      </div>
+  )
+}
+
+export default Login
