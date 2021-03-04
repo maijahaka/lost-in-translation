c5d9e33af3d865f8df3e477f373b7725d25daba0
Maija Haka <maija.haka@live.fi>
2021-03-04 12:23:27 +0200

    feat: add logout button to profile page and implement logout functionality

--- src/components/containers/Profile.js
+++ src/components/containers/Profile.js
@@ -1,11 +1,20 @@
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
 import Translations from '../translations/Translations'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import AppHeader from '../shared/AppHeader'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from 'react-bootstrap/Button'
+import { useHistory } from 'react-router-dom'
 
 function Profile() {
+  let history = useHistory()
+
   const user = window.localStorage.getItem('user')
+
+  const handleLogoutClick = () => {
+    window.localStorage.clear()
+    history.push('/')
+  }
 
   return (
     <div>
@@ -16,6 +25,7 @@ function Profile() {
           <p>Here are your ten most recent translations listed below.</p>
         </Jumbotron>
         <Translations />
+        <Button onClick={handleLogoutClick}>Logout</Button>
       </Container>
     </div>
   )
