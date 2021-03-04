0a5183bd71c5016b8b9465e4caf1ea0a8ec39ea9
Maija Haka <maija.haka@live.fi>
2021-03-04 11:48:57 +0200

    feat: show username on the profile page

--- src/components/containers/Profile.js
+++ src/components/containers/Profile.js
@@ -5,12 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import AppHeader from '../shared/AppHeader'
 
 function Profile() {
+  const user = window.localStorage.getItem('user')
+
   return (
     <div>
       <AppHeader />
       <Container>
         <Jumbotron>
-          <h1>Hello, {"yourName"}</h1>
+          <h1>Hello, {user && user}</h1>
           <p>Here are your ten most recent translations listed below.</p>
         </Jumbotron>
         <Translations />
